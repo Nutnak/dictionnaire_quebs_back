9508901af4b684bbfaa7c2547eeeb73b8e26e4a7
Quentin <quentinucme@hotmail.fr>
2025-04-10 12:30:57 +0200

    :fix: just fix the CRUD for definition model

--- src/models/definition.js
+++ src/models/definition.js
@@ -6,7 +6,11 @@ const schema = new Schema({
     name: String,
     definition: String,
     exemple: String,
-    pseudonyme: String
+    pseudonyme: String,
+    isvisible: {
+        type: Boolean,
+        default: false
+    },
 })
 
 const definition = mongoose.model('definition', schema);
